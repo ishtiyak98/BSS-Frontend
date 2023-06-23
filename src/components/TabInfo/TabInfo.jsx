cbbbb3221397f25bf37b98712135c9b229e2f6a3
ishtiyak98 <rifatishtiyak@gmail.com>
2023-06-24 00:57:06 +0600

    react-tab with custom css

--- src/components/TabInfo/TabInfo.jsx
+++ src/components/TabInfo/TabInfo.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+
 import "./TabInfo.scss";
 
 function TabInfo() {
   return (
     <section className="tab-info-container">
-      <h2 className="title">What are Clinical Trials?</h2>
+      <h2 className="title">What are clinical trials?</h2>
 
       <p className="para-text">
         Clinical Trials are medical research studies that help to find out
@@ -19,6 +21,30 @@ function TabInfo() {
       <p className="para-text">
         There are four main phases of clinical studies:
       </p>
+      <br />
+      <div className="tab-container">
+        <Tabs>
+          <TabList className="tab-title-container">
+            <Tab>Phase 1</Tab>
+            <Tab>Phase 2</Tab>
+            <Tab>Phase 3</Tab>
+            <Tab>Phase 4</Tab>
+          </TabList>
+
+          <TabPanel>
+            <div>Any content 1</div>
+          </TabPanel>
+          <TabPanel>
+            <div>Any content 2</div>
+          </TabPanel>
+          <TabPanel>
+            <div>Any content 3</div>
+          </TabPanel>
+          <TabPanel>
+            <div>Any content 4</div>
+          </TabPanel>
+        </Tabs>
+      </div>
     </section>
   );
 }
