34e070ed6696747336f8b4dc6ac7875ed46f83a7
ishtiyak98 <rifatishtiyak@gmail.com>
2023-06-23 23:25:17 +0600

    vite with tab-info

---
+++ src/components/TabInfo/TabInfo.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./TabInfo.scss";
+
+function TabInfo() {
+  return (
+    <section className="tab-info-container">
+      <h2 className="title">What are Clinical Trials?</h2>
+
+      <p className="para-text">
+        Clinical Trials are medical research studies that help to find out
+        whether a treatment works and is safe for use before it can be
+        distributed to the whole population. Regulatory agencies review the
+        results from these studies and make an independent decision on whether a
+        treatment can be approved for use. All Galapagos clinical studies are
+        performed according to government regulations that are in place to help
+        protect the rights of anyone taking part in a trial.
+      </p>
+      <br />
+      <p className="para-text">
+        There are four main phases of clinical studies:
+      </p>
+    </section>
+  );
+}
+
+export default TabInfo;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TabInfo from "../../components/TabInfo/TabInfo";
+
+function Home() {
+  return (
+    <>
+      <TabInfo></TabInfo>
+    </>
+  );
+}
+
+export default Home;
