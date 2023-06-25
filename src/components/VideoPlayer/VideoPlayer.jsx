b4273730e80ea49869373f5aaf7353b3c6c856ce
ishtiyak98 <rifatishtiyak@gmail.com>
2023-06-25 21:31:54 +0600

    assignment done

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -28,17 +28,56 @@ function VideoPlayer() {
                 </div>
               </div>
               <div className="videoPlayer__carousel__content__text">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Reiciendis enim placeat cum quo culpa totam omnis deleniti nam
-                expedita maxime!
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Reiciendis enim placeat cum quo culpa totam omnis deleniti nam
+                  expedita maxime!
+                </p>
+                <p className="font-semibold">
+                  Ron formal clinical trial <br /> participant
+                </p>
               </div>
             </div>
           </div>
           <div>
-            <h3>2</h3>
+            <div className="videoPlayer__carousel__content">
+              <div className="videoPlayer__carousel__content__img">
+                <img src={VideoThumbLg} alt="" className="w-[100%] h-[100%]" />
+                <div className="videoPlayer__carousel__content__img__playButton">
+                  <img src={PlayButton} alt="" className="w-[100%] h-[100%]" />
+                </div>
+              </div>
+              <div className="videoPlayer__carousel__content__text">
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Reiciendis enim placeat cum quo culpa totam omnis deleniti nam
+                  expedita maxime!
+                </p>
+                <p className="font-semibold">
+                  Ron formal clinical trial <br /> participant
+                </p>
+              </div>
+            </div>
           </div>
           <div>
-            <h3>3</h3>
+            <div className="videoPlayer__carousel__content">
+              <div className="videoPlayer__carousel__content__img">
+                <img src={VideoThumbLg} alt="" className="w-[100%] h-[100%]" />
+                <div className="videoPlayer__carousel__content__img__playButton">
+                  <img src={PlayButton} alt="" className="w-[100%] h-[100%]" />
+                </div>
+              </div>
+              <div className="videoPlayer__carousel__content__text">
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Reiciendis enim placeat cum quo culpa totam omnis deleniti nam
+                  expedita maxime!
+                </p>
+                <p className="font-semibold">
+                  Ron formal clinical trial <br /> participant
+                </p>
+              </div>
+            </div>
           </div>
         </Slider>
       </div>
